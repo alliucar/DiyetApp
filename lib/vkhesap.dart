a32d1bb29eada1fd6569b808d3904f68691b2b9a
alliucar <aliucar@aliucar.com.tr>
2021-04-27 20:31:20 +0300

    vücut kitle endeksi güncellendi

--- lib/vkhesap.dart
+++ lib/vkhesap.dart
@@ -41,188 +41,185 @@ class _VkHesapState extends State {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
       backgroundColor: Colors.white,
       drawer: MyDrawer(),
       body: Builder(
-        builder: (context) => Column(
-          children: [
-            Row(
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(top: 50.0, left: 10.0),
-                  child: Image(
-                    image: AssetImage("assets/images/logo.png"),
+        builder: (context) => SingleChildScrollView(
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(top: 50.0, left: 10.0),
+                    child: Image(
+                      image: AssetImage("assets/images/logo.png"),
+                    ),
                   ),
-                ),
-                Expanded(
-                  child: Container(
-                    width: 0,
+                  Expanded(
+                    child: Container(
+                      width: 0,
+                    ),
                   ),
-                ),
-                InkWell(
-                  child: Container(
-                      margin: EdgeInsets.only(top: 50.0, right: 8.0),
+                  InkWell(
+                    child: Container(
+                        margin: EdgeInsets.only(top: 50.0, right: 8.0),
+                        decoration: const ShapeDecoration(
+                          color: Color(0xffF0F3F5),
+                          shape: CircleBorder(),
+                        ),
+                        child: IconButton(
+                          icon: Icon(
+                            Icons.notifications_none,
+                          ),
+                          iconSize: 20,
+                          onPressed: () {
+                            Navigator.pushNamed(context, "/bildirimler");
+                          },
+                        )),
+                  ),
+                  Container(
+                      margin: const EdgeInsets.only(top: 50.0, right: 10.0),
                       decoration: const ShapeDecoration(
                         color: Color(0xffF0F3F5),
                         shape: CircleBorder(),
                       ),
                       child: IconButton(
                         icon: Icon(
-                          Icons.notifications_none,
+                          Icons.menu,
+                        ),
+                        iconSize: 20,
+                        onPressed: () => Scaffold.of(context).openDrawer(),
+                      )),
+                ],
+              ),
+              Row(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(top: 10.0, left: 10.0),
+                    child: Text(
+                      "Vücut Kitle Endeksi Hesaplama",
+                      textDirection: TextDirection.rtl,
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                        color: Color(0xffC85D72),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      width: 0,
+                    ),
+                  ),
+                  Container(
+                      margin: EdgeInsets.only(top: 10.0, right: 8.0),
+                      child: IconButton(
+                        icon: Icon(
+                          Icons.navigate_before,
                         ),
                         iconSize: 20,
                         onPressed: () {
-                          Navigator.pushNamed(context, "/bildirimler");
+                          Navigator.pushNamed(context, "/main");
                         },
                       )),
-                ),
-                Container(
-                    margin: const EdgeInsets.only(top: 50.0, right: 10.0),
-                    decoration: const ShapeDecoration(
-                      color: Color(0xffF0F3F5),
-                      shape: CircleBorder(),
-                    ),
-                    child: IconButton(
-                      icon: Icon(
-                        Icons.menu,
-                      ),
-                      iconSize: 20,
-                      onPressed: () => Scaffold.of(context).openDrawer(),
-                    )),
-              ],
-            ),
-            Row(
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(top: 10.0, left: 10.0),
-                  child: Text(
-                    "Vücut Kitle Endeksi Hesaplama",
-                    textDirection: TextDirection.rtl,
-                    textAlign: TextAlign.left,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18,
-                      color: Color(0xffC85D72),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Container(
-                    width: 0,
-                  ),
-                ),
-                Container(
-                    margin: EdgeInsets.only(top: 10.0, right: 8.0),
-                    child: IconButton(
-                      icon: Icon(
-                        Icons.navigate_before,
-                      ),
-                      iconSize: 20,
-                      onPressed: () {
-                        Navigator.pushNamed(context, "/main");
-                      },
-                    )),
-              ],
-            ),
-            Row(
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.only(top: 10.0, left: 10.0),
-                      child: Image(
-                        image: AssetImage("assets/images/tartihesap.png"),
-                      ),
-                    ),
-                    Container(
-                      padding: EdgeInsets.all(10.0),
-                      constraints: BoxConstraints(maxWidth: 370),
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(8.0)),
-                      child: TextField(
-                        controller: boyText,
-                        keyboardType: TextInputType.number,
-                        maxLength: 3,
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'Boyunuz',
+                ],
+              ),
+              Row(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(top: 10.0, left: 10.0),
+                        child: Image(
+                          image: AssetImage("assets/images/tartihesap.png"),
                         ),
                       ),
-                    ),
-                    Container(
-                      padding: EdgeInsets.all(10.0),
-                      constraints: BoxConstraints(maxWidth: 370),
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(8.0)),
-                      child: TextField(
-                        controller: kiloText,
-                        keyboardType: TextInputType.number,
-                        maxLength: 3,
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'Kilonuz',
+                      Container(
+                        padding: EdgeInsets.all(10.0),
+                        constraints: BoxConstraints(maxWidth: 370),
+                        decoration: BoxDecoration(borderRadius: BorderRadius.circular(8.0)),
+                        child: TextField(
+                          controller: boyText,
+                          keyboardType: TextInputType.number,
+                          maxLength: 3,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Boyunuz',
+                          ),
                         ),
                       ),
-                    ),
-                    Container(
-                      padding: EdgeInsets.all(8.0),
-                      constraints: BoxConstraints(maxWidth: 370),
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(8.0)),
-                      child: Padding(
+                      Container(
+                        padding: EdgeInsets.all(10.0),
+                        constraints: BoxConstraints(maxWidth: 370),
+                        decoration: BoxDecoration(borderRadius: BorderRadius.circular(8.0)),
+                        child: TextField(
+                          controller: kiloText,
+                          keyboardType: TextInputType.number,
+                          maxLength: 3,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Kilonuz',
+                          ),
+                        ),
+                      ),
+                      Container(
                         padding: EdgeInsets.all(8.0),
-                        child: SizedBox(
-                          height: 50,
-                          width: 350,
-                          child: ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              primary: Color(0xffC85D72), // background
-                              onPrimary: Colors.white, // foreground
+                        constraints: BoxConstraints(maxWidth: 370),
+                        decoration: BoxDecoration(borderRadius: BorderRadius.circular(8.0)),
+                        child: Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: SizedBox(
+                            height: 50,
+                            width: 350,
+                            child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                primary: Color(0xffC85D72), // background
+                                onPrimary: Colors.white, // foreground
+                              ),
+                              onPressed: () {
+                                Hesapla(int.parse(boyText.text), int.parse(kiloText.text));
+                                print(sonuc);
+                              },
+                              child: Text(' Hesapla'),
                             ),
-                            onPressed: () {
-                              Hesapla(int.parse(boyText.text),
-                                  int.parse(kiloText.text));
-                              print(sonuc);
-                            },
-                            child: Text(' Hesapla'),
                           ),
                         ),
                       ),
-                    ),
-                    Container(
-                      decoration: BoxDecoration(
-                          color: Color(0xffFFFFFF),
-                          borderRadius: BorderRadius.all(Radius.circular(10))),
-                      height: 50,
-                      width: 168,
-                      margin: EdgeInsets.only(
-                        left: 10.0,
-                        right: 5.0,
+                      Container(
+                        decoration: BoxDecoration(color: Color(0xffFFFFFF), borderRadius: BorderRadius.all(Radius.circular(10))),
+                        height: 50,
+                        margin: EdgeInsets.only(
+                          left: 10.0,
+                          right: 5.0,
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.only(top: 20.0),
+                            ),
+                            Text(
+                              sonuc,
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                                color: Color(0xff4D565B),
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(top: 5.0),
+                            ),
+                          ],
+                        ),
                       ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Padding(
-                            padding: EdgeInsets.only(top: 20.0),
-                          ),
-                          Text(sonuc,  style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            color: Color(0xff4D565B),
-                          ),),
-                          Padding(
-                            padding: EdgeInsets.only(top: 5.0),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ],
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
